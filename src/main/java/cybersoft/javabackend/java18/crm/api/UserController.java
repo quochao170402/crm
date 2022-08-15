7db600d82fb4c5b7f061aa0bc69941ef9591652f
Quốc Hào <hao.dev1704@gmail.com>
2022-08-15 08:42:04 +0700

    First commit

--- src/main/java/cybersoft/javabackend/java18/crm/api/UserController.java
+++ src/main/java/cybersoft/javabackend/java18/crm/api/UserController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "userController",
         urlPatterns = {
                 UrlUtils.USER,
         })
 public class UserController extends HttpServlet {
-
     private UserService userService;
     private Gson gson;
 
@@ -31,5 +31,9 @@ public class UserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String json = gson.toJson(userService.findAll());
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write(json);
+            writer.flush();
+        }
     }
 }
--- src/main/java/cybersoft/javabackend/java18/crm/repository/impl/UserRepositoryImpl.java
+++ src/main/java/cybersoft/javabackend/java18/crm/repository/impl/UserRepositoryImpl.java
@@ -5,6 +5,7 @@ import cybersoft.javabackend.java18.crm.repository.AbstractRepository;
 import cybersoft.javabackend.java18.crm.repository.UserRepository;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,18 @@ public class UserRepositoryImpl extends AbstractRepository<UserModel> implements
             PreparedStatement statement = connection.prepareStatement(query);
             List<UserModel> userModels = new ArrayList<>();
 
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                UserModel userModel = new UserModel();
+                userModel.setId(results.getInt("id"));
+                userModel.setEmail(results.getString("email"));
+                userModel.setPassword(results.getString("password"));
+                userModel.setFullname(results.getString("fullname"));
+                userModel.setAvatar(results.getString("avatar"));
+                userModel.setRoleId(results.getInt("role_id"));
+
+                userModels.add(userModel);
+            }
             return userModels;
 
         });
